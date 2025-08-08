02062f97828cd7aaf077ac2e93e5bf0fd3ebea6a
Smile_112 <Lnowikow@mail.ru>
2025-08-08 21:57:32 +0300

    fix for MVP Aspect system 2

--- port-1.20.1/src/main/java/thaumcraft/scan/AspectDataManager.java
+++ port-1.20.1/src/main/java/thaumcraft/scan/AspectDataManager.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
